04aadebded9480cf1535f3450cbc00389551df92
Sateesh Kumar D <sateesh.d@arisglobal.com>
2022-06-18 14:40:21 +0530

    Initial commit.

---
+++ src/test/java/com/training/cs/CloudConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.training.cs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
